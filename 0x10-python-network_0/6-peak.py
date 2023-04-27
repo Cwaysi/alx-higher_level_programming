cc4b55d968aa982e971a3b88ddf734ea1a7d2cae
Cwaysi Morison <cwaysimorison@outlook.com>
2023-04-27 09:19:26 -0700

    mina

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 """Defines a peak-finding algorithm."""
 
+
 def find_peak(list_of_integers):
-   
-   """     int: The peak value in the list."""
-    if not list_of_integers:
+    """lets Return a peak in a list when unsorted"""
+    if list_of_integers == []:
         return None
 
-    left = 0
-    right = len(list_of_integers) - 1
+    wait = len(list_of_integers)
+    if wait == 1:
+        return list_of_integers[0]
+    elif wait == 2:
+        return max(list_of_integers)
 
-    while left < right:
-        mid = (left + right) // 2
-
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            left = mid + 1
-        else:
-            right = mid
-
-    return list_of_integers[left]
+    mid = int(wait / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
