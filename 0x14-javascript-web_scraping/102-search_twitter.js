2e861ccbcb85b8109304538e3e29f2d51e9e1be1
Justice Morison Gurah <cwaysimorison@outlook.com>
2023-05-23 12:52:16 +0000

    Update 102

--- 0x14-javascript-web_scraping/102-search_twitter.js
+++ 0x14-javascript-web_scraping/102-search_twitter.js
@@ -1,47 +1,43 @@
 #!/usr/bin/node
-const base64 = require('base-64');
+
 const request = require('request');
-const utf8 = require('utf8');
 
-let promise = new Promise(function (resolve, reject) {
-  const token = utf8.decode(base64.encode(`${process.argv[2]}:${process.argv[3]}`));
-  const options = {
-    url: 'https://api.twitter.com/oauth2/token',
-    headers: {
-      Authorization: `Basic ${token}`,
-      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-    },
-    form: {
-      grant_type: 'client_credentials'
-    }
-  };
-  request.post(options, function (error, response, body) {
-    if (!error) {
-      resolve(JSON.parse(body).access_token);
-    }
-  });
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const movieData = JSON.parse(body);
+    const characterUrls = movieData.characters;
+
+    const fetchCharacterData = (url) => {
+      return new Promise((resolve, reject) => {
+        request(url, (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else {
+            const characterData = JSON.parse(body);
+            resolve(characterData.name);
+          }
+        });
+      });
+    };
+
+    const fetchAllCharacters = async () => {
+      try {
+        const characterNames = await Promise.all(
+          characterUrls.map((url) => fetchCharacterData(url))
+        );
+        characterNames.forEach((name) => {
+          console.log(name);
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchAllCharacters();
+  }
 });
-
-promise.then(
-  result => search(result),
-  error => console.log(error)
-);
-
-function search (bearer) {
-  const options = {
-    url: 'https://api.twitter.com/1.1/search/tweets.json',
-    headers: {
-      Authorization: `Bearer ${bearer}`
-    },
-    qs: {
-      q: process.argv[4],
-      count: '5'
-    }
-  };
-  request.get(options, function (error, response, body) {
-    if (!error) {
-      const tweets = JSON.parse(body).statuses;
-      tweets.forEach((t) => console.log(`[${t.id}] ${t.text} by ${t.user.name}`));
-    }
-  });
-}
